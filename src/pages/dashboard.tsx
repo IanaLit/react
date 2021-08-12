b462120c21c0cbcba8c56c0bb52bf583404166d2
iana <litvinova.iana1212@gmail.com>
2021-08-12 12:22:22 +0700

    feat: add loader

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { AxiosResponse } from 'axios';
 import React, { ChangeEvent, FC, useState , MouseEvent, useEffect } from 'react';
 import { ArticleCard } from '../components/card/articleCard';
+import { Loader } from '../components/loader/loader';
 import { Pagination } from '../components/pagination/pagination';
 import SearchPanel from '../components/search-panel/search-panel';
 import { Article, GET200_Articles, SortType } from '../interfaces/articleInterface';
@@ -9,19 +10,20 @@ import axios from '../services/api';
 const API_KEY = '13f832d3d3244deb8442164a5f9263af';
 
 export const Dashboard: FC = () => {
-    const [searchValue, setSearchValue] = useState<string>('');
+    const [searchValue, setSearchValue] = useState<string>('all');
     const [articles, setArticles] = useState<Article[]>([]);
     const [sortBy, setSortBy] = useState<SortType>(SortType.popularity);
     const [page, setPage] = useState(1);
     const [pageSize, setPageSize] = useState(5);
     const [totalResults, setTotalResults] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         handleSubmit('');
     }, [page]);
     const handleSubmit = async (e: ChangeEvent<HTMLFormElement>|'') => {
         if(e)e.preventDefault();
-        //setIsLoading(true);
+        setIsLoading(true);
         try {
             const response: AxiosResponse<GET200_Articles> = await axios.get(
                 `v2/everything?q=${searchValue}&apiKey=${API_KEY}&sortBy=${sortBy}&page=${page}&pageSize=${pageSize}`,
@@ -32,7 +34,7 @@ export const Dashboard: FC = () => {
         } catch (err: any) {
             console.error(e);
         } finally {
-            //setIsLoading(false);
+            setIsLoading(false);
         }
     };
         
@@ -47,7 +49,7 @@ export const Dashboard: FC = () => {
         setPage(page);
     }
     return (
-        <div>
+        <div className='dashboard'>
             <SearchPanel
                 searchValue={searchValue}
                 sortBy={sortBy}
@@ -55,7 +57,8 @@ export const Dashboard: FC = () => {
                 handleSubmit={handleSubmit}
                 handleFilter={handleFilter}
 
-            />
+            /><Loader/>
+            {/* {isLoading ? : 
             <div className="cards">
                 {articles.map((article, index: number) => {
                     return (
@@ -69,7 +72,7 @@ export const Dashboard: FC = () => {
                         urlToImage={article.urlToImage}
                 />)
             })}
-            </div>
+            </div>} */}
             <Pagination page={page} pageSize={pageSize} totalResults={totalResults} gotoPage={ gotoPage}/>
         </div>
     )
