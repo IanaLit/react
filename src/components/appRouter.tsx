8b5320ff32b7c3d6596d61d9ca3501eb3bc359e1
iana <litvinova.iana1212@gmail.com>
2021-08-17 12:38:19 +0700

    feat: add animation

--- src/components/appRouter.tsx
+++ src/components/appRouter.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import { About } from '../pages/about';
 import { Dashboard } from '../pages/dashboard';
 import { DetailArticle } from '../pages/detail/detailArticle';
 import { NotFound } from '../pages/notFound/notFound';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import '../style.scss';
 
-export const AppRouter = () => (
-  <Switch>
-    <Route exact path="/about">
-      <About />
-    </Route>
-    <Route path="/error">
-      <NotFound />
-    </Route>
-    <Route exact path="/">
-      <Dashboard />
-    </Route>
-    {/* /:urlToImage/:description/:author/:publishedAt */}
-    <Route exact path="/details/:title">
-      <DetailArticle />
-    </Route>
-    <Redirect to="/error" />
-  </Switch>
-);
+export const AppRouter = () => {
+  const location = useLocation();
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        classNames="fade"
+        timeout={300}
+      >
+        <Switch location={location}>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route path="/error">
+            <NotFound />
+          </Route>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route exact path="/details/:title">
+            <DetailArticle />
+          </Route>
+          <Redirect to="/error" />
+        </Switch>
+      </CSSTransition>
+      </TransitionGroup>
+  );
+}
